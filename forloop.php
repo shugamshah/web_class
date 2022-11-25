1e340fb755531b46d1c69b307ece3faf03fe3629
shugamshah <106583216+shugamshah@users.noreply.github.com>
2022-11-25 09:51:34 +0545

    Add files via upload

---
+++ forloop.php
@@ -0,0 +1,23 @@
+<?php
+$num = 3;
+ 
+switch ($num) {
+    case 1:
+        echo "Sorry!! best of luck for next time";
+        break;
+    case 2:
+        echo "You have won the lottry";
+        break;
+    case 3:
+        echo "You have won the lottry";
+        break;
+    case 1:
+        echo "Sorry!! best of luck for next time";
+        break;
+    case 1:
+        echo "Sorry!! best of luck for next time";
+        break;
+        default:
+        echo "Your number is not mentioned";
+}
+?>
