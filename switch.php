5436473a040de0c467180470fec1e02c5bedf7df
shugamshah <106583216+shugamshah@users.noreply.github.com>
2022-11-25 09:50:15 +0545

    Add files via upload

---
+++ switch.php
@@ -0,0 +1,20 @@
+<?php
+$num=3;
+
+switch ($num) {
+    case 1:
+        echo" your faviorate number is 1";
+        break;
+    case 2:
+        echo"your faviorate number is 2";
+        break;
+    case 3:
+        echo ";your faviorate number is 3";
+        break;
+    default:
+        echo"you don't have faviorate numebr ";
+
+
+
+
+}
